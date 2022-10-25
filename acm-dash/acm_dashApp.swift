85dbd015762ebe1088670226117bc99a4424bc7f
Ryan <ry.roche@icloud.com>
2022-10-25 12:20:39 -0500

    Initial Commit

---
+++ acm-dash/acm_dashApp.swift
@@ -0,0 +1,17 @@
+//
+//  acm_dashApp.swift
+//  acm-dash
+//
+//  Created by Ryan Roche on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct acm_dashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
