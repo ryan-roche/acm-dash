8fd446a9e84744bafdd9e13b41badc5eef7022d6
Ryan <ry.roche@icloud.com>
2022-11-10 12:57:00 -0600

    Added App Icon Files

--- acm-dash/MyView.swift
+++ acm-dash/MyView.swift
@@ -7,20 +7,20 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MyView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Test String")
         }
         .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MyView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MyView()
     }
 }
--- acm-dash/acm_dashApp.swift
+++ acm-dash/acm_dashApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct acm_dashApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MyView()
         }
     }
 }
